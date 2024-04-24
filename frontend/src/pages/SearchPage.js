0cea2e44236c4543d195236d521835c616fca7ac
Steven Maddela <70993825+stevenmaddela@users.noreply.github.com>
2024-04-24 15:14:06 -0500

    Update SearchPage.js

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -424,14 +424,8 @@ const handleSentimentViewClick = () => {
 // Custom function to format date in 'YYYY-MM-DD' format
 const formatDate = (dateString) => {
   const date = new Date(dateString);
-  const year = date.getFullYear();
-  let month = '' + (date.getMonth() + 1);
-  let day = '' + date.getDate();
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-
-  return [year, month, day].join('-');
+  const isoDate = date.toISOString().split('T')[0]; // Get ISO date without time
+  return isoDate;
 };
 
 
